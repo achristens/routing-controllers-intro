60068ac3e3cb4470d18908b458a1001111aef569
Abby Christens <achristens@gmail.com>
2017-10-11 14:09:17 -0400

    Adding header variables to each method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
 
   def welcome
+    @header = "Hey Ya'll"
   end
 
   def about
+    @header = "You Think You Know, But You Have No Idea"
   end
 
   def contest
+    @header = "LET'S WIN SOME STUFF"
   end
 end
