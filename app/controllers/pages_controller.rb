9526ffe1c3b8ac2aff219c7889fe7b9369f400ed
Abby Christens <achristens@gmail.com>
2017-10-11 14:34:23 -0400

    Creating secret page with magic word

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -26,4 +26,13 @@ class PagesController < ApplicationController
     requested_size = params[:size]
     @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
+
+  def secrets
+    if
+      params[:magic_word] == "merino"
+    else
+      flash[:alert] = "That is not the correct magic word..."
+      redirect_to "/welcome"
+    end
+  end
 end
