91b9a6f98e6f55985b003fedc914838612e359f7
Abby Christens <achristens@gmail.com>
2017-10-11 14:19:25 -0400

    Creating dynamic url for Kitten view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,4 +11,9 @@ class PagesController < ApplicationController
   def contest
     @header = "LET'S WIN SOME STUFF"
   end
+
+  def kitten
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
 end
